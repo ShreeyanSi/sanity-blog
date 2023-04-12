81f44e0381342df018c90e9bdd06ebda2e79609f
ShreeyanSi <shreeyansimkhada@gmail.com>
2023-04-12 18:11:06 +0545

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Homepage() {
+    return (
+        <div>
+
+            <h1 className="text-4xl">Welcome to the blog</h1>
+            <p>Some Content</p>
+        </div>
+
+    )
+}
